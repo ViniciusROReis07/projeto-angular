cf0b62162b793e224c82126800ee5c71eefe5de8
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-03-08 22:07:54 -0300

    feat: Home Spotify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProdutoComponent } from './pages/produto/produto.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'produto', component: ProdutoComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdutoComponent } from './pages/produto/produto.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProdutoComponent
+    ProdutoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
