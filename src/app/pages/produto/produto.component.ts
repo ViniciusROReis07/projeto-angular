ffe5a49b09b04deb1cb3c4fc9f0167420abebab7
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-03-08 21:06:11 -0300

    Hello word

---
+++ src/app/pages/produto/produto.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto',
+  templateUrl: './produto.component.html',
+  styleUrls: ['./produto.component.scss']
+})
+export class ProdutoComponent implements OnInit {
+
+  public title:string = 'Hello word';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
